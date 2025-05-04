f79588e97cd1beb1b76560741004695dfb96ce07
Avenze <alexander.pettersson@thefrostcloud.com>
2025-05-04 23:54:51 +0200

    tweak(assembly): update assembly information for build process

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://github.com/Avenze")]
 [assembly: AssemblyProduct("rpf2fivem")]
-[assembly: AssemblyCopyright("Copyright © Frostcloud Development 2023")]
+[assembly: AssemblyCopyright("Copyright © Frostcloud 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
